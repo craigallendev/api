1b63e1074f2d803c0484cada8c30c03be357d6bd
Craig Allen <callen7@hotmail.co.uk>
2024-06-26 20:23:19 +0000

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-https://ci-jshint.herokuapp.com/api?api_key=1IWr57og6vldY8i_aKFzOaD91sc
+const API_KEY = "1IWr57og6vldY8i_aKFzOaD91sc";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
+}
